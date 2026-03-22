9e580121bb29298d1500de3567a42ff36117abf8
joergsflow <joergsflow@gmail.com>
2026-03-22 19:56:07 +0100

    fix: single XAML ResourceDictionary with x:Class and InitializeComponent
    - All DataTemplates now inline in ResourceDictionary.xaml with proper
      x:Class pointing to SkwResourceDictionary partial class
    - Code-behind uses InitializeComponent() (WPF standard pattern)
    - Emptied individual template XAML files to prevent type resolution errors
    - Previous approach (programmatic pack URI loading) was silently failing
      and preventing the plugin from loading in NINA

--- plugin/NINA.AstroCircular.SkyWaver/Resources/ResourceDictionary.xaml.cs
+++ plugin/NINA.AstroCircular.SkyWaver/Resources/ResourceDictionary.xaml.cs
@@ -4,24 +4,9 @@ using System.Windows;
 namespace NINA.AstroCircular.SkyWaver.Resources {
 
     [Export(typeof(ResourceDictionary))]
-    public class SkwResourceDictionary : ResourceDictionary {
+    public partial class SkwResourceDictionary : ResourceDictionary {
         public SkwResourceDictionary() {
-            // Load merged dictionaries programmatically instead of XAML InitializeComponent
-            MergedDictionaries.Add(new ResourceDictionary {
-                Source = new System.Uri("pack://application:,,,/NINA.AstroCircular.SkyWaver;component/Resources/OptionsTemplate.xaml")
-            });
-            MergedDictionaries.Add(new ResourceDictionary {
-                Source = new System.Uri("pack://application:,,,/NINA.AstroCircular.SkyWaver;component/Resources/SkwDefocusTemplate.xaml")
-            });
-            MergedDictionaries.Add(new ResourceDictionary {
-                Source = new System.Uri("pack://application:,,,/NINA.AstroCircular.SkyWaver;component/Resources/SkwCircularCaptureTemplate.xaml")
-            });
-            MergedDictionaries.Add(new ResourceDictionary {
-                Source = new System.Uri("pack://application:,,,/NINA.AstroCircular.SkyWaver;component/Resources/SkwIntegrateFramesTemplate.xaml")
-            });
-            MergedDictionaries.Add(new ResourceDictionary {
-                Source = new System.Uri("pack://application:,,,/NINA.AstroCircular.SkyWaver;component/Resources/SkwCollimationRunTemplate.xaml")
-            });
+            InitializeComponent();
         }
     }
 }
